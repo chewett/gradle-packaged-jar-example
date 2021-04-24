0bcd96dbe892ff003e5bd5e0e8a4de76f1276ae6
Christopher Hewett <chewett@hotmail.co.uk>
2021-04-24 13:03:33 +0100

    Minor tweaks to code to add a comment on the main class

--- src/main/java/net/chewett/github/gradlepackagedjarexample/GradleImportTest.java
+++ src/main/java/net/chewett/github/gradlepackagedjarexample/GradleImportTest.java
@@ -5,6 +5,10 @@ import com.google.gson.Gson;
 
 public class GradleImportTest {
 
+    /**
+     * Simple main function that loads the google Gson library and converts it to an array of ints.
+     * @param args
+     */
     public static void main(String[] args) {
         Gson gson = new Gson();
         int[] ints2 = gson.fromJson("[1,2,3,4,5]", int[].class);
