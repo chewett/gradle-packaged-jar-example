218f22249ed16859f0565d913c68697bce10b22b
Christopher Hewett <chewett@hotmail.co.uk>
2021-04-24 12:58:42 +0100

    Fix typo

--- src/main/java/net/chewett/github/gradlepackagedjarexample/GradleImportTest.java
+++ src/main/java/net/chewett/github/gradlepackagedjarexample/GradleImportTest.java
@@ -8,7 +8,7 @@ public class GradleImportTest {
     public static void main(String[] args) {
         Gson gson = new Gson();
         int[] ints2 = gson.fromJson("[1,2,3,4,5]", int[].class);
-        System.out.println("JSON int array conveted from a string by Gson");
+        System.out.println("JSON int array converted from a string by Gson");
         System.out.println(Arrays.toString(ints2));
     }
 }
